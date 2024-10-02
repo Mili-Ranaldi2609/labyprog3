2378bfa5cc8809fe3a2ae9009aa0241013878b41
unknown <mranaldi@gmail.com>
2024-10-02 10:52:12 -0300

    proyecto giphy terminado

---
+++ giphyApp/giphyApp/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { GiftList } from "./components/giftList/GiftList"
+import { NavBar } from "./components/ui/NavBar/NavBar"
+
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <GiftList />
+    </>
+  )
+}
+
+export default App
